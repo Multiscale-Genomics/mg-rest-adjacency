726153353d3b97f8d9fb431a7d282a907c646091
markmcdowall <mark.mcdowall@gmail.com>
2016-07-19 10:14:55 +0100

    Config files for running tests

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='rest',
+    packages=['rest'],
+    include_package_data=True,
+    install_requires=[
+        'flask', 'flask_restful', 'numpy', 'h5py'
+    ],
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'pytest',
+    ],
+)
