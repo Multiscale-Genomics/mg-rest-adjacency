feb0a46400d84f692972b2e56ac1827470888216
Mark McDowall <mark.mcdowall@gmail.com>
2016-12-12 15:46:48 +0000

    Added the waitress module to the list of requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     packages=['rest'],
     include_package_data=True,
     install_requires=[
-        'flask', 'flask_restful', 'numpy', 'h5py'
+        'flask', 'flask_restful', 'numpy', 'h5py', 'waitress'
     ],
     setup_requires=[
         'pytest-runner',
