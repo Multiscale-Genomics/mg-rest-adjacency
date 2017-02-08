6c74d2d398e10093bd39ade708c4698bf570fcc5
Mark McDowall <mark.mcdowall@gmail.com>
2017-02-08 16:41:54 +0000

    Reversed the logic for the links

--- rest/hdf5_reader.py
+++ rest/hdf5_reader.py
@@ -106,7 +106,7 @@ class hdf5:
                 y_start = (i[1]-chr_param[y_chr]["bins"][resolution][1])*int(resolution)
             
             r = {"chrA": chr_id, "startA": x_start, "chrB": y_chr, "startB": y_start, "value": int(result[i[0],i[1]])}
-            if no_links != None:
+            if no_links == None:
                 r['_links'] = {'self': value_url + "?user_id=" + str(user_id) + "&file_id=" + str(file_id) + "&res=" + str(resolution) + "&pos_x=" + str(i[0]+x+xy_offset) + "&pos_y=" + str(i[1])}
             results.append(r)
         
