eb36b86d8421ae6412e476da95707e3558112653
Mark McDowall <mark.mcdowall@gmail.com>
2017-06-15 16:06:48 +0100

    Updated the unit test codes. Requires new datasets to be generated before adding t the repo

--- test/test_rest.py
+++ test/test_rest.py
@@ -23,50 +23,71 @@ from context import app
 
 @pytest.fixture
 def client(request):
-    db_fd, app.app.config['DATABASE'] = tempfile.mkstemp()
-    app.app.config['TESTING'] = True
-    client = app.app.test_client()
+    db_fd, app.APP.config['DATABASE'] = tempfile.mkstemp()
+    app.APP.config['TESTING'] = True
+    client = app.APP.test_client()
     
     def teardown():
         os.close(db_fd)
-        os.unlink(app.app.config['DATABASE'])
+        os.unlink(app.APP.config['DATABASE'])
     request.addfinalizer(teardown)
 
     return client
 
-def test_taxon_id(client):
-    rv = client.get('/rest/v0.0/getInteractions')
-    assert b'9606' in rv.data
+def test_endpoints(client):
+    rv = client.get('/mug/api/adjacency')
+    print rv.data
+    assert b'_links' in rv.data
 
-def test_accession_id(client):
-    rv = client.get('/rest/v0.0/getInteractions/9606')
-    assert b'GCA_000001405.14' in rv.data
+def test_ping(client):
+    rv = client.get('/mug/api/adjacency/ping')
+    print rv.data
+    assert b'status' in rv.data
 
-def test_datasets(client):
-    rv = client.get('/rest/v0.0/getInteractions/9606/GCA_000001405.14')
-    assert b'rao2014' in rv.data
+def test_details(client):
+    rv = client.get('/mug/api/adjacency/details')
+    details = json.loads(rv.data)
+    print details
+    assert 'usage' in details
 
-def test_resolutions(client):
-    rv = client.get('/rest/v0.0/getInteractions/9606/GCA_000001405.14/rao2014')
-    assert b'resolution' in rv.data
-    assert b'10000' in rv.data
+def test_details_00(client):
+    rv = client.get('/mug/api/adjacency/details?user_id=test&file_id=test')
+    details = json.loads(rv.data)
+    print details
+    assert 'resolutions' in details
 
-def test_chromosomes(client):
-    rv = client.get('/rest/v0.0/getInteractions/9606/GCA_000001405.14/rao2014/10000')
-    assert b'1' in rv.data
-    assert b'X' in rv.data
-    assert b'Y' in rv.data
+def test_details_01(client):
+    rv = client.get('/mug/api/adjacency/details?user_id=test&file_id=test')
+    details = json.loads(rv.data)
+    print details
+    assert len(details['resolutions']) == 3
 
-def test_chromosome(client):
-    rv = client.get('/rest/v0.0/getInteractions/9606/GCA_000001405.14/rao2014/10000/1')
-    assert b'bins' in rv.data
-    assert b'size' in rv.data
+def test_getInteractions(client):
+    rv = client.get('/mug/api/adjacency/getInteractions')
+    details = json.loads(rv.data)
+    print details
+    assert 'usage' in details
 
-def test_range(client):
-    rv = client.get('/rest/v0.0/getInteractions/9606/GCA_000001405.14/rao2014/10000/1/0/10000')
-    assert b'startA' in rv.data
-    assert b'startB' in rv.data
+def test_getInteractions_00(client):
+    rv = client.get('/mug/api/adjacency/getInteractions?user_id=test&file_id=test&chr=19&res=1000000&start=1000000&end=5000000')
+    details = json.loads(rv.data)
+    print details.keys()
+    assert 'values' in details
 
-def test_value(client):
-    rv = client.get('/rest/v0.0/getValue/9606/GCA_000001405.14/rao2014/10000/1/1')
-    assert b'46' in rv.data
+def test_getInteractions_01(client):
+    rv = client.get('/mug/api/adjacency/getInteractions?user_id=test&file_id=test&chr=19&res=1000000&start=1000000&end=5000000')
+    details = json.loads(rv.data)
+    print len(details['values'])
+    assert len(details['values']) == 11408
+
+def test_getValue(client):
+    rv = client.get('/mug/api/adjacency/getValue')
+    details = json.loads(rv.data)
+    print details
+    assert 'usage' in details
+
+def test_getValue_00(client):
+    rv = client.get('/mug/api/adjacency/getValue?user_id=test&file_id=test&res=1000000&pos_x=0&pos_y=2673')
+    details = json.loads(rv.data)
+    print details
+    assert 'values' in details
