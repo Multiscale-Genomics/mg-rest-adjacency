e42c0b30538287497e9568c2536f2668344cbe41
Mark McDowall <mark.mcdowall@gmail.com>
2017-06-28 09:13:22 +0100

    Unit test script stub for testing the reader. This functionality should eventually be moved into the DM API as a core feature

---
+++ test/test_reader.py
@@ -0,0 +1,34 @@
+"""
+.. Copyright 2017 EMBL-European Bioinformatics Institute
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
+from __future__ import print_function
+
+#import os
+#import tempfile
+#import json
+import pytest
+
+from rest.hdf5_reader import hdf5
+
+def test_range():
+    """
+    Test the range function
+    """
+    hdf5_handle = hdf5('test', '')
+    x = hdf5_handle.get_range(10000, 'chr1', 100000, 200000, limit_chr='chr2')
+
+    assert 'results' in x
+    assert len(x['results']) > 0
