bd2c37c1bafe25fa7c6a378ebae3a891ec22755a
Mark McDowall <mark.mcdowall@gmail.com>
2017-06-30 13:34:15 +0100

    Changes so that the dmp module is

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['pyBigWig']
+MOCK_MODULES = ['pyBigWig', 'dmp']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 import dmp
