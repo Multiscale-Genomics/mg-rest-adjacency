92aafec6834ae7f10a11c228a0402baf9a722a66
Mark McDowall <mark.mcdowall@gmail.com>
2017-10-31 16:54:12 +0000

    Removed h5py and numpy from teh setup so that they are handled as requirements

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     packages=['rest'],
     include_package_data=True,
     install_requires=[
-        'flask', 'flask_restful', 'numpy', 'h5py', 'waitress', 'pylint', 'pytest'
+        'flask', 'flask_restful', 'waitress', 'pylint', 'pytest'
     ],
     setup_requires=[
         'pytest-runner',
